fc97472e65c6776f7f85563174965e77bcd94483
Ilya M <ilayvm@gmail.com>
2021-03-30 00:24:45 +0300

    add ChooseCategoryDialog

--- app/src/main/java/ru/skillbranch/skillarticles/ui/dialogs/ChoseCategoryDialog.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/dialogs/ChoseCategoryDialog.kt
@@ -3,11 +3,39 @@ package ru.skillbranch.skillarticles.ui.dialogs
 import androidx.appcompat.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.Adapter
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import androidx.paging.PagedList
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.fragment_choose_dialog.*
+import kotlinx.android.synthetic.main.fragment_choose_dialog.view.*
+import kotlinx.android.synthetic.main.item_category.*
+import kotlinx.android.synthetic.main.item_category.view.*
+import kotlinx.coroutines.*
+import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.data.local.entities.CategoryData
+import ru.skillbranch.skillarticles.data.models.CommentItemData
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
+import ru.skillbranch.skillarticles.ui.articles.ArticleVH
+import ru.skillbranch.skillarticles.ui.custom.CommentItemView
 import ru.skillbranch.skillarticles.viewmodels.articles.ArticlesViewModel
 
+/*
 class ChoseCategoryDialog : DialogFragment() {
     private val viewModel: ArticlesViewModel by activityViewModels()
     private val selectedCategories = mutableListOf<String>()
@@ -37,3 +65,4 @@ class ChoseCategoryDialog : DialogFragment() {
         return adb.create()
     }
 }
+*/
